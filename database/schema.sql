f4465ce14e9c7051fe144699f45ebd9eb8fe7e0c
devfahimbd <learncodewithfahim@gmail.com>
2026-05-17 13:23:17 +0600

    minor update at 2026-05-17 13:23:16

--- database/schema.sql
+++ database/schema.sql
@@ -168,3 +168,5 @@ INSERT INTO categories (name, slug, type) VALUES
 ('Lecture Notes', 'lecture-notes', 'resource'),
 ('Books', 'books', 'resource'),
 ('Software', 'software', 'resource');
+
+-- update at 2026-05-17 13:23:16
