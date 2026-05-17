1494d1d8ab3fd28f4968907767978fb62258dbbc
devfahimbd <learncodewithfahim@gmail.com>
2026-05-17 14:41:52 +0600

    auto commit at 2026-05-17 14:41:51

--- control-panel/categories/edit.php
+++ control-panel/categories/edit.php
@@ -2,3 +2,5 @@
 $id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
 header('Location: ../' . basename(__DIR__) . '.php' . ($id ? '?action=edit&id=' . $id : ''));
 exit;
+
+// auto-update at 2026-05-17 14:41:51
