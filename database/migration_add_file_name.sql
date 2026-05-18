152fc0d6cdb10b87400baa5b88985c3a906c6922
devfahimbd <learncodewithfahim@gmail.com>
2026-05-18 08:24:51 +0600

    improve request handler

--- database/migration_add_file_name.sql
+++ database/migration_add_file_name.sql
@@ -3,3 +3,5 @@
 -- This allows storing the original uploaded filename for proper download naming
 
 ALTER TABLE resources ADD COLUMN file_name VARCHAR(255) AFTER file_path;
+
+-- improved documentation clarity
